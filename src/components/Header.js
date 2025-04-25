06743af8614c57f303aa3d2a98a8a0e484a2a7c1
Peter Bwire <peterbwirea@gmail.com>
2025-04-25 12:00:31 +0300

    Adds header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import {Navbar, Container} from "react-bootstrap";
+
+const Header = () => {
+  return <Navbar bg="dark" variant="dark" style={{height: 80}} >
+    <Container>
+      <Navbar.Brand>
+        <a href="/">Shopping Cart</a>
+      </Navbar.Brand>
+    </Container>
+  </Navbar>
+}
+
+export default Header
