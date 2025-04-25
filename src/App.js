92a7c157f7f84a767ffef2771a7280faf290ce3e
Peter Bwire <peterbwirea@gmail.com>
2025-04-25 15:12:31 +0300

    fixes the app file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import Context from './context/Context';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Context> {/* Wrap everything with Context provider */}
+      <BrowserRouter>
+        <Header />
+        <Routes> {/* Use Routes instead of direct Route */}
+          <Route path='/' element={<Home />} /> {/* New v6 syntax */}
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </Context>
   );
 }
 
